a185bfe29661961c15cd5ebdc20a996bfea80613
divyansh panchal <panchalsonu662@gmail.com>
2025-05-16 05:05:38 -0700

    voting age criteria c prgramming file created successfully

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int age;
+    printf("enter the age: ");
+    scanf("%d",&age);
+    
+if(age>=21){
+    printf("eligible for voting");
+    }
+else{
+    printf("not eligible for voting");
+}
+
+}
